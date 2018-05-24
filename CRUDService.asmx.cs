0dffb8c8071c1a3c846ce888bea1217a24722ff9
Nivi3535 <34634623+Nivi3535@users.noreply.github.com>
2018-05-24 12:18:21 +0930

    Add files via upload

---
+++ CRUDService.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Project2.Models;
+namespace Project2
+{
+    /// <summary>
+    /// Summary description for CRUDService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+     [System.Web.Script.Services.ScriptService]
+    public class CRUDService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string PostEmployee(Employee emp)
+        {
+            return "Success";
+        }
+       [WebMethod]
+    public string GetCurrentTime(String name)
+        {
+            String message = DateTime.Now.ToString();
+            return message;
+        }
+        
+    }
+}
---
+++ Service1.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Project2.Models;
+
+    ///<summary>
+    /// Summary description for Service
+    ///</summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+    [System.Web.Script.Services.ScriptService]
+
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        public Service1()
+        {
+            //Uncomment the following line if using designed components
+            //InitializeComponent();
+        }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "A";
+    }
+        [WebMethod]
+        public string GetCurrentTime(string name)
+        {
+            string message = "Hello ";
+            message += name;
+            message += "\nCurrent Time: ";
+            message += DateTime.Now.ToString();
+            return message;
+        }
+    }
